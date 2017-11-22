f397b41ae67b5c1bc268e5f6c67d80143f4615bc
sochin.jiang <johnpub@zju.edu.cn>
2017-11-22 21:15:22 +0800

    feat: add gpointer-convertion macros

---
+++ basic-types/type-conversion-macros.c
@@ -0,0 +1,21 @@
+#include <glib.h>
+#include <stdio.h>
+
+/*
+//only preserve 32 bits of the integer; values outside the range of a 32-bit integer will be mangled.
+#define             GINT_TO_POINTER(i)      ((gpointer) (glong) (i))
+#define             GPOINTER_TO_INT(p)      ((gint)  (glong) (p))
+
+#define             GUINT_TO_POINTER(u)     ((gpointer) (gulong) (u))
+#define             GPOINTER_TO_UINT(p)     ((guint) (gulong) (p))
+#define             GSIZE_TO_POINTER(s)     ((gpointer) (gsize) (s))
+#define             GPOINTER_TO_SIZE(p)     (gsize) (p))
+
+ */
+
+
+int main()
+{
+
+    return 0;
+}
