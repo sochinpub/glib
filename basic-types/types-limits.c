dcae7d46b0805a00684f1922178185ee1baf1753
sochin.jiang <johnpub@zju.edu.cn>
2017-11-22 20:39:46 +0800

    feat: add types-limits macros

---
+++ basic-types/types-limits.c
@@ -0,0 +1,44 @@
+/*
+
+#define             G_MININT           INT_MIN 
+#define             G_MAXINT           INT_MAX
+#define             G_MAXUINT          UINT_MAX
+
+#define             G_MINSHORT         SHRT_MIN   
+#define             G_MAXSHORT         SHRT_MAX   
+#define             G_MAXUSHORT        SHRT_MAX
+
+#define             G_MINLONG          LONG_MIN    
+#define             G_MAXLONG          LONG_MAX    
+#define             G_MAXULONG         ULONG_MAX
+
+#define             G_MININT8          ((gint8) 0x80)    
+#define             G_MAXINT8          ((gint8) 0x7f)    
+#define             G_MAXUINT8         ((guint8) 0xff)
+
+#define             G_MININT16         ((gint16) 0x8000)
+#define             G_MAXINT16         ((gint16) 0x7fff)
+#define             G_MAXUINT16        ((guint16) 0xffff)
+
+#define             G_MININT32         ((gint32) 0x80000000)
+#define             G_MAXINT32         ((gint32) 0x7fffffff)
+#define             G_MAXUINT32        ((guint32) 0xfffffff)
+
+#define             G_MININT64         ((gint64) G_GINT64_CONSTANT(0x8000000000000000))
+#define             G_MAXINT64         G_GINT64_CONSTANT(0x7fffffffffffffff)
+#define             G_MAXUINT64        G_GINT64_CONSTANT(0xffffffffffffffffU)
+        
+#define             G_MAXSIZE          G_MAXULONG
+#define             G_MINSSIZE         G_MINLONG
+#define             G_MAXSSIZE         G_MAXLONG
+
+#define             G_MINOFFSET        G_MININT64
+#define             G_MAXOFFSET        G_MAXINT64
+    
+#define             G_MINFLOAT         FLT_MIN
+#define             G_MAXFLOAT         FLT_MAX
+
+#define             G_MINDOUBLE        DBL_MIN
+#define             G_MAXDOUBLE        DBL_MAX
+
+*/
